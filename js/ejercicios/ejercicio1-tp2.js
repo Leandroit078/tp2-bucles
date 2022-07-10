1a1622e1818f18acd0a75738929f9c92f8e4d2bd
Leandro Villafañe <leandro.villafane.utn@gmail.com>
2022-07-09 21:47:48 -0300

    hice el ejercicio numero 1

---
+++ js/ejercicios/ejercicio1-tp2.js
@@ -0,0 +1,11 @@
+let edad = parseInt(prompt('Ingrese su edad'));
+
+console.log(edad);
+
+
+if(edad >= 18) { 
+    document.write('Usted ya puede conducir');
+}
+else {
+    document.write('Usted es menor de edad '); 
+}
