cd7aad6af5eab12d92615ff324078ca7afb3d65d
Leandro Villafañe <leandro.villafane.utn@gmail.com>
2022-07-09 21:38:57 -0300

    hicimos teoria de bucles en clases

---
+++ js/bucles.js
@@ -0,0 +1,51 @@
+
+
+/*
+while(condicion logica){
+   todas las lineas de codigo que quiero que repita el programa
+   cambiar la condicion logica
+}
+
+
+
+let mates = 1;
+
+while(mates <= 20){
+    document.write("<br>preparar el mate y pasarlo al siguiente compañero, mate cebado n: "+ mates);
+    mates= mates+1; // esto es igual a cantidas++ // cantidad+= cantidad, esto es que se sume acumulativamente
+}
+
+*/
+
+
+
+/* bucle do-while hacer ... mientras
+
+do{
+    todo el codigo que quiero ejecutar x cantidad de veces
+    cambiar la condicion logica
+}while(condicion logica)
+
+
+let mate = 21;
+
+do{
+    document.write("<br>preparar mate y entregarlo al siguente compañero, mate cebadon: " + mate);
+    mate++
+}while(mate <= 20) 
+
+*/
+ 
+
+
+/* bulce for
+
+for(inicializar una variable ; condicion logica ; incrementar o decrementar la variable declarada al inicio){
+    todo el codigo a ejercutar repetidas veces
+}
+
+
+for(let mate = 1; mate <= 20; mate = mate+1){
+    document.write("<br>preparar mate y entregarlo al siguente compañero, mate cebado n: " + mate);
+}
+*/
