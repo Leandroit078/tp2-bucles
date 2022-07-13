6ae67877bbec40212d06545776c5ba85c3256d55
Leandro Villafañe <leandro.villafane.utn@gmail.com>
2022-07-13 20:35:29 -0300

    ya me funciona el ejercicio 4: no funcionaba por el scope de let y tuve que usar var

--- js/ejercicios/ejercicio4-tp2.js
+++ js/ejercicios/ejercicio4-tp2.js
@@ -1,15 +1,14 @@
-let total = 0;
+var suma = 0;
 
 do {
-    let numero = prompt("Introduce un numero para sumar");
-
-    if (number(numero) == numero) {
-        numero = number(numero);
-        total = total + numero;
+    var numero = prompt("Introduce un numero");
+    if (Number(numero) == numero) {
+        numero = Number(numero);
+        suma = suma + numero;
+    } else {
+        if (numero != undefined) {
+            alert(numero + " No es un numero");
+        }
     }
-    else if (numero !== undefined) {
-        alert(numero + "no es un valor numerico valido");
-    }
-}
-while (numero !== undefined);
-document.write(" La suma de los numeros ingresados es: " + total);
+} while (numero != undefined);
+document.write(suma);
